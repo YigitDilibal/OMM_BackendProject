42d2e97a56a9b87d7fdb31ce268291589181f21c
Sumeyra Develi <sumeyra_dvli@outlook.com>
2025-05-15 21:32:55 +0300

    US26_GET

--- src/test/java/stepdefinitions/API_Stepdefinitions.java
+++ src/test/java/stepdefinitions/API_Stepdefinitions.java
@@ -256,4 +256,41 @@ public class API_Stepdefinitions {
     @When("Then The api user sets {string} path parameters.")
     public void thenTheApiUserSetsPathParameters(String arg0) {
     }
+
+    @And("The api user validates the {string}, {string}, {string}, {string}, {string}, {string} contents of the data in the response body in mystaffs.")
+    public void theApiUserValidatesTheContentsOfTheDataInTheResponseBodyInMystaffs(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5) {
+
+        response.then()
+                .assertThat()
+                .body("data.staff_list[9].provider_id", containsString(arg0),
+                        "data.staff_list[9].first_name", containsString(arg1),
+                        "data.staff_list[9].last_name", equalTo(arg2),
+                        "data.staff_list[9].country_code", containsString(arg3),
+                        "data.staff_list[9].contact_no", containsString(arg4),
+                        "data.staff_list[9].email", containsString(arg5));
+        response.prettyPrint();
+
+    }
+
+    @And("The api user validates the {string}, {string}, {string}, {string} , {string}, {string} ,{string} contents of the data in the response body.")
+    public void theApiUserValidatesTheContentsOfTheDataInTheResponseBody(String arg0, String arg1, String arg2, String arg3, String arg4, String arg5, String arg6) {
+
+        response.then()
+                .assertThat()
+                .body("data.staff_list[9].dob", containsString(arg0),
+                        "data.product_list[9].gender", containsString(arg1),
+                        "data.staff_list[9].profile_img", containsString(arg2),
+                        "data.staff_list[9].designation", equalTo(arg3),
+                        "data.staff_list[9].exp_year", equalTo(arg4),
+                        "data.staff_list[9].exp_month", containsString(arg5),
+                        "data.staff_list[9].status", containsString(arg6));
+    }
+
+    @And("The api user prepares a GET request body for {string} to send to the api myStaffs endpoint")
+    public void theApiUserPreparesAGETRequestBodyForToSendToTheApiMyStaffsEndpoint(String Id) {
+
+        jsonObjectRequest.put("Id",Id);
+    }
+
+
 }
